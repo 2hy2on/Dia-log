d3574d10f464a842be835706777faf6131be4cfd
Hanminjoo72 <hmjhaha0207@naver.com>
2023-12-14 19:23:21 +0900

    로그인 마이페이지 연결

---
+++ src/main/java/controller/login/MypageController.java
@@ -0,0 +1,16 @@
+package controller.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class MypageController implements Controller {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 여기에 글쓰기 관련 로직을 추가할 수 있습니다.
+
+        // 글쓰기가 완료된 후에는 다시 목록 페이지로 이동하도록 지정
+        return "/login/Mypage.jsp";  // 또는 다른 페이지로 리다이렉트하도록 변경 가능
+    }
+}
